38c479844626c2e5dabafeefb3934dc4debab00e
橡皮擦 <2506152074@qq.com>
2024-06-05 15:07:43 +0800

    初始化项目

---
+++ frontend/src/main.ts
@@ -0,0 +1,7 @@
+import { router } from './router/router';
+import { createApp } from 'vue'
+import './style.css'
+import 'virtual:uno.css'
+import App from './App.vue'
+
+createApp(App).use(router).mount('#app')
