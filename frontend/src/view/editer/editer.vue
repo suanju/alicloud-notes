1b0d7fd32a820345306ebe2940dcaeaeb51ebd1e
suanju <2506152074@qq.com>
2024-06-27 10:39:08 +0800

    同步代码

---
+++ frontend/src/view/editer/editer.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+        <QuillEditor theme="snow" />
+    </div>
+</template>
+
+<script setup lang="ts">
+import { QuillEditor } from '@vueup/vue-quill'
+import '@vueup/vue-quill/dist/vue-quill.snow.css';
+
+components: {
+    QuillEditor
+}
+
+</script>
